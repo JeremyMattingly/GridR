a677788720bc7b0e44cdd1b5ef690317d88588b3
Jeremy Mattingly <jmattingly@acculynx.com>
2024-11-10 11:56:11 -0600

    Now that Subregion is responsible for determining the subregions of a grid, we can ensure that nothing outside of Subregion can create a subregion.

--- Core.Test/GridTests.cs
+++ Core.Test/GridTests.cs
@@ -55,11 +55,50 @@
         [TestMethod]
         public void Constructor_ExampleArray_ProperlyFindsSubregions()
         {
-            var (input, threshold) = Grid.GetExampleArray();
+            var (exampleArray, threshold) = Grid.GetExampleArray();
+            var expectedCellsForInput = ExpectedCellsForExampleArray.Cells;
 
-            var result = new Grid(input, threshold);
+            var expectedSubregions = new Dictionary<uint, Dictionary<Coordinate, Cell>>();
+
+            var subregion1 = new Dictionary<Coordinate, Cell>
+            {
+                { new(1, 1), expectedCellsForInput[new(1, 1)] }
+            };
+            expectedSubregions.Add(0, subregion1);
+
+            var subregion2 = new Dictionary<Coordinate, Cell>
+            {
+                { new(3, 3), expectedCellsForInput[new(3, 3)] },
+                { new(3, 2), expectedCellsForInput[new(3, 2)] },
+                { new(4, 2), expectedCellsForInput[new(4, 2)] },
+                { new(4, 1), expectedCellsForInput[new(4, 1)] },
+                { new(5, 1), expectedCellsForInput[new(5, 1)] }
+            };
+            expectedSubregions.Add(1, subregion2);
+
+            var subregion3 = new Dictionary<Coordinate, Cell>
+            {
+                { new(5, 4), expectedCellsForInput[new Coordinate(5, 4)] },
+                { new(5, 5), expectedCellsForInput[new Coordinate(5, 5)] }
+            };
+            expectedSubregions.Add(2, subregion3);
+
+            var result = new Grid(exampleArray, threshold);
 
             Assert.IsTrue(result.Subregions.Count == 3);
+
+            foreach (var expectedSubregion in expectedSubregions)
+            {
+                Assert.IsTrue(result.Subregions.ContainsKey(expectedSubregion.Key));
+
+                Assert.AreEqual(expectedSubregion.Value.Count, result.Subregions[expectedSubregion.Key].Cells.Count);
+
+                foreach (var expectedCell in expectedSubregion.Value.Values)
+                {
+                    Assert.IsTrue(result.Subregions[expectedSubregion.Key].Cells.ContainsKey(expectedCell.GridCoordinate)
+                                  && result.Subregions[expectedSubregion.Key].Cells[expectedCell.GridCoordinate].Equals(expectedCell));
+                }
+            }
         }
     }
 }
--- Core/Subregion.cs
+++ Core/Subregion.cs
@@ -2,20 +2,20 @@
 
 namespace ActivTrak.Assessment.GridR.Core
 {
-    public class Subregion : IEquatable<Subregion>
+    public class Subregion
     {
         private readonly Dictionary<Coordinate, Cell> _cells = [];
 
         public uint Id { get; }
         public ReadOnlyDictionary<Coordinate, Cell> Cells { get; private set; }
 
-        public Subregion(uint id)
+        private Subregion(uint id)
         {
             Id = id;
             Cells = _cells.AsReadOnly();
         }
 
-        public static Dictionary<uint, Subregion> GetSubregionsForGrid(Grid grid)
+        internal static Dictionary<uint, Subregion> GetSubregionsForGrid(Grid grid)
         {
             Dictionary<uint, Subregion> subregionsToReturn = [];
 
@@ -192,29 +192,6 @@ namespace ActivTrak.Assessment.GridR.Core
         private void AddCell(Coordinate coordinate, Cell cell)
         {
             _cells.Add(coordinate, cell);
-        }
-
-        public bool Equals(Subregion? other) => other != null && other.Id == Id;
-
-        public override bool Equals(object? obj)
-        {
-            return Equals(obj as Subregion);
-        }
-
-        public override int GetHashCode()
-        {
-            throw new NotImplementedException();
-        }
-
-        public static bool operator ==(Subregion? left, Subregion? right)
-        {
-            if (left is null || right is null) return Object.Equals(left, right);
-            return left.Id == right.Id;
-        }
-
-        public static bool operator !=(Subregion? left, Subregion? right)
-        {
-            return !(left == right);
         }
 
         public override string ToString()
